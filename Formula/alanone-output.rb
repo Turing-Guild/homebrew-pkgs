88392de8dfdd76e9fa9d837d8e4aefa02a613ae0
Ian Pratt <ianjdpratt@gmail.com>
2024-07-03 11:06:28 -0500

    update alanone-output to version 1.0.5

--- Formula/alanone-output.rb
+++ Formula/alanone-output.rb
@@ -2,9 +2,9 @@ require "language/node"
 
 class AlanoneOutput < Formula
 	desc "The CLI that interfaces with the Alan One computer"
-	homepage "https://github.com/turing-guild/alanone-output"
-	url "https://registry.npmjs.org/@turingguild/alanone-output/-/alanone-output-1.0.4.tgz"
-	sha256 "148e580e4ed5e612d0c8e726207c5cb49979493b05e80fac3de36b5419d14435"
+	homepage "https://www.npmjs.com/package/@turingguild/alanone-output"
+	url "https://registry.npmjs.org/@turingguild/alanone-output/-/alanone-output-1.0.5.tgz"
+	sha256 "31ee323ac70c1ba9f2237a5461f121bbf1cd09633783524bc1162b61c16336b5"
 	license "ISC" # or the appropriate license
   
 	depends_on "node"
--- Formula/alanone.rb
+++ Formula/alanone.rb
@@ -2,7 +2,7 @@ require "language/node"
 
 class Alanone < Formula
 	desc "The CLI that interfaces with the Alan One computer"
-	homepage "https://github.com/turing-guild/alanone"
+	homepage "https://www.npmjs.com/package/@turingguild/alanone"
 	url "https://registry.npmjs.org/@turingguild/alanone/-/alanone-1.1.5.tgz"
 	sha256 "7baa46a29dd5de3af321a56207edbc0a03f157c1fabd31eebbdf6ebf1a4a1e2a"
 	license "ISC" # or the appropriate license
