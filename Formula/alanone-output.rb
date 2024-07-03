9bee3d758966d74378fa08db85f597bd1143ae6e
Ian Pratt <ianjdpratt@gmail.com>
2024-07-02 22:14:17 -0500

    small fixes

--- Formula/alanone-output.rb
+++ Formula/alanone-output.rb
@@ -1,8 +1,8 @@
 require "language/node"
 
 class AlanoneOutput < Formula
-	desc "The CLI that interfaces with the Alan One computer."
-	homepage "https://github.com/turing-guild/alanone"
+	desc "The CLI that interfaces with the Alan One computer"
+	homepage "https://github.com/turing-guild/alanone-output"
 	url "https://registry.npmjs.org/@turingguild/alanone-output/-/alanone-output-1.0.0.tgz"
 	sha256 "b8a3ebe8337edca9e5fdbe7a0ea5dfb5f3c58e558a319ab056d74869c1c174f7"
 	license "ISC" # or the appropriate license
@@ -10,11 +10,11 @@ class AlanoneOutput < Formula
 	depends_on "node"
   
 	def install
-	  system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-	  bin.install_symlink Dir["#{libexec}/bin/*"]
+		system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+		bin.install_symlink Dir["#{libexec}/bin/*"]
 	end
   
 	test do
-	  system "#{bin}/alanone-output"
+		system "#{bin}/alanone-output"
 	end
-  end
+end
--- Formula/alanone.rb
+++ Formula/alanone.rb
@@ -1,7 +1,7 @@
 require "language/node"
 
 class Alanone < Formula
-	desc "The CLI that interfaces with the Alan One computer."
+	desc "The CLI that interfaces with the Alan One computer"
 	homepage "https://github.com/turing-guild/alanone"
 	url "https://registry.npmjs.org/@turingguild/alanone/-/alanone-1.1.5.tgz"
 	sha256 "7baa46a29dd5de3af321a56207edbc0a03f157c1fabd31eebbdf6ebf1a4a1e2a"
@@ -10,11 +10,11 @@ class Alanone < Formula
 	depends_on "node"
   
 	def install
-	  system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-	  bin.install_symlink Dir["#{libexec}/bin/*"]
+		system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+		bin.install_symlink Dir["#{libexec}/bin/*"]
 	end
   
 	test do
-	  system "#{bin}/alanone"
+		system "#{bin}/alanone"
 	end
-  end
+end
