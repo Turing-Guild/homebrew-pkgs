33dbdf5a4eec5f9066e6e97a035d00510702a5cc
Kevin McQuown <kevin@thewcl.com>
2025-08-03 16:57:33 -0500

    updates

--- Formula/alanone.rb
+++ Formula/alanone.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Alanone < Formula
   desc "The CLI that interfaces with the Alan One computer"
   homepage "https://www.npmjs.com/package/@turingguild/alanone"
-  url "https://registry.npmjs.org/@turingguild/alanone/-/alanone-1.4.2.tgz"
-  sha256 "26125ab0bbc2695d8a51cf513b9f6dc96488ec0119d69db96a6b27e73b4af832"
+  url "https://registry.npmjs.org/@turingguild/alanone/-/alanone-1.4.3.tgz"
+  sha256 "ea71a7cb14422cf4208fa358d245cdb37841f19ca710a1a54add26626f862030"
   license "ISC"
 
   depends_on "node"
