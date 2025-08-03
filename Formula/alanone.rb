67702afcc47c2a35bb9cc2c36c7756f52a2619ef
Kevin McQuown <kevin@thewcl.com>
2025-08-03 15:13:53 -0500

    try again

--- Formula/alanone.rb
+++ Formula/alanone.rb
@@ -1,19 +1,22 @@
+require "language/node"
+
 class Alanone < Formula
   desc "The CLI that interfaces with the Alan One computer"
   homepage "https://www.npmjs.com/package/@turingguild/alanone"
   url "https://registry.npmjs.org/@turingguild/alanone/-/alanone-1.4.1.tgz"
-  version "1.4.1"
+  sha256 "142a2b7feaae1d4c269910b02102ee3497862d52b534e479b7378a5f77f83f9c"
   license "ISC"
 
   depends_on "node"
 
+  # Tell Homebrew explicitly: we don't need a compiler
   def install
-    system "npm", "install", "-g", "@turingguild/alanone@#{version}"
-    bin.install_symlink Dir[HOMEBREW_PREFIX/"lib/node_modules/@turingguild/alanone/bin/*"]
+    ENV["HOMEBREW_NO_COMPILER_CHECK"] = "1"
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 
   test do
     system "#{bin}/alanone", "--version"
   end
 end
-
